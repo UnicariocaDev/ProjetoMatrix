a3cf79625a94008cb0f1df5cb235cf079a4273f9
Jonatan Machado <jonatan.mac@gmail.com>
2017-10-09 19:28:41 -0300

    some customisation on page

--- js/plugins.js
+++ js/plugins.js
@@ -21,170 +21,170 @@ URL: https://themeforest.net/user/dupinder15
 9 - Owl Carousel in Testimonials Section
 10 - Launching Google map
 ----------------------------------------------------- */
-(function($) {
-	          "use strict";
+(function ($) {
+	"use strict";
 
-	         /* ---------------------------------------------------
-		        1 - Loading Page 
-	         ----------------------------------------------------- */
-	          $(window).on("load", function() {
-	          $(".loader").fadeOut("slow");
-              });
-	          $(window).on("load", function() {
-	          $(".loader-wrapper").slideUp("slow");
-              });
+	/* ---------------------------------------------------
+	   1 - Loading Page 
+	----------------------------------------------------- */
+	$(window).on("load", function () {
+		$(".loader").fadeOut("slow");
+	});
+	$(window).on("load", function () {
+		$(".loader-wrapper").slideUp("slow");
+	});
 
-             /* ---------------------------------------------------
-		        2 - Navigation Menu Scroll Controls
-	         ----------------------------------------------------- */
-              $("#aboutme-btn").on("click", function() {
-		      $("html, body").animate({
-			    scrollTop: $(".about-me").offset().top-89
-		                              }, 700);
-	          });	
-	          $("#services-btn").on("click", function() {
-		      $("html, body").animate({
-			    scrollTop: $(".services").offset().top-89
-		                              }, 700);
-	          });	
-			  $("#testimonials-btn").on("click", function() {
-		      $("html, body").animate({
-			    scrollTop: $(".testimonials").offset().top-89
-		                              }, 700);
-	          });
-			  $("#experience-btn").on("click", function() {
-		      $("html, body").animate({
-			    scrollTop: $(".experience").offset().top-89
-		                              }, 700);
-	          });	
-			  	
-			  $("#portfolio-btn").on("click", function() {
-		      $("html, body").animate({
-			    scrollTop: $(".portfolio").offset().top-89
-		                              }, 700);
-	          });
-			  $("#contactme-btn").on("click", function() {
-		      $("html, body").animate({
-			    scrollTop: $(".contact").offset().top-89
-		                              }, 700);
-	          });
-			  
-	         /* -------------------------------------------------------------
-		       3 - Typewriter Effect Controls
-	         --------------------------------------------------------------- */
-              $("#typing-effect").typed({
-              strings: ["I am John Doe", "I Design Websites"],//Change Page Heading Here
-              typeSpeed: 30,
-		      backSpeed: 30,
-		      loop:true,
-		      showCursor:false
-              });
-		   
-	         /* -------------------------------------------------------------
-		        4 - Move to Portfolio section on clicking view more button 
-	         --------------------------------------------------------------- */
-		      $("#portfolio-btn-2").on("click", function() {
-		      $("html, body").animate({
-		      scrollTop: $(".portfolio").offset().top-89
-		                              }, 700);
-	          });
-		   
-	         /* -------------------------------------------------------------
-		        5 - Move to About-me section on clicking animated buttons
-	         --------------------------------------------------------------- */
-		      $("#animated-btn-1").on("click", function() {
-		      $("html, body").animate({
-		      scrollTop: $(".about-me").offset().top-89
-		                              }, 700);
-	          });
-		      $("#animated-btn-2").on("click", function() {
-		      $("html, body").animate({
-		      scrollTop: $(".about-me").offset().top-89
-		                              }, 700);
-	          });
-			  
-			 /* -------------------------------------------------------------
-		        6 - Move to Contact section on clicking Hire Me Button
-	         --------------------------------------------------------------- */
-			  $("#hireme-btn").on("click", function() {
-		      $("html, body").animate({
-			    scrollTop: $(".contact").offset().top-90
-		                              }, 700);
-	          });
-			  
-	         /* ---------------------------------------------------
-		        7 - MixItUp Plugin in Portfolio Section
-	         ----------------------------------------------------- */
-	          $(".projects").mixItUp();
-			  
-			 /* ---------------------------------------------------
-		         8 - Contact Form Validation
-	         ----------------------------------------------------- */
-	          var contactForm = $("#contact-form"),
-		      submitBtn = $(".submit-btn"),
-		      formResponse = $(".form-response");
-	          contactForm.validator().on("submit", function (e) {
-			  if(e.isDefaultPrevented()) {
-				formResponse.text("Sorry, you didn't fill the form.").fadeIn(1000);
-			  } else {
-				e.preventDefault();
-				submitForm();
-			  }
-		      });
-		      // Submit Form
-		      function submitForm() {
-			  // Some Variables
-			  var name = $("#name").val(),
-				mail = $("#mail").val(),
-				message = $("#message").val();
-			  // Ajax    
-			  $.ajax({
-				type: "POST",
-				url: "php/contact.php",
-				data: "name=" + name + "&mail=" + mail + "&message=" + message,
-				beforeSend: function(text) {
-					submitBtn.html("Sending...");
-					formResponse.fadeOut(500).text("");
-				},
-				success: function (text) {
-					if(text == "success") {
-						contactForm[0].reset();
-						formResponse.text("Thanks! Your message sent correctly.").fadeIn(1000);
-						submitBtn.html("Send Message");
-					} else {
-						formResponse.text(text).fadeIn(1000);
-					}
-				   }
-			      });
-		        }
-		        // Moving placeholder on focus on any input in contact-me section //
-	            $(".contact .form-control").focusout(function() {
-		        var textValue = $(this).val();
-		        if (textValue === "") {
-			    $(this).removeClass("has-value");
-		        } else {
-			    $(this).addClass("has-value");
-		        }
-	            });
-  
+	/* ---------------------------------------------------
+	   2 - Navigation Menu Scroll Controls
+	----------------------------------------------------- */
+	$("#aboutme-btn").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".about-me").offset().top - 89
+		}, 700);
+	});
+	$("#services-btn").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".services").offset().top - 89
+		}, 700);
+	});
+	$("#testimonials-btn").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".testimonials").offset().top - 89
+		}, 700);
+	});
+	$("#experience-btn").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".experience").offset().top - 89
+		}, 700);
+	});
+
+	$("#portfolio-btn").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".portfolio").offset().top - 89
+		}, 700);
+	});
+	$("#contactme-btn").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".contact").offset().top - 89
+		}, 700);
+	});
+
+	/* -------------------------------------------------------------
+	  3 - Typewriter Effect Controls
+	--------------------------------------------------------------- */
+	$("#typing-effect").typed({
+		strings: ["Somos o Projeto Matrix", "Transformamos vidas"],//Change Page Heading Here
+		typeSpeed: 80,
+		backSpeed: 15,
+		loop: true,
+		showCursor: false
+	});
+
+	/* -------------------------------------------------------------
+	   4 - Move to Portfolio section on clicking view more button 
+	--------------------------------------------------------------- */
+	$("#portfolio-btn-2").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".portfolio").offset().top - 89
+		}, 700);
+	});
+
+	/* -------------------------------------------------------------
+	   5 - Move to About-me section on clicking animated buttons
+	--------------------------------------------------------------- */
+	$("#animated-btn-1").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".about-me").offset().top - 89
+		}, 700);
+	});
+	$("#animated-btn-2").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".about-me").offset().top - 89
+		}, 700);
+	});
+
+	/* -------------------------------------------------------------
+	   6 - Move to Contact section on clicking Hire Me Button
+	--------------------------------------------------------------- */
+	$("#hireme-btn").on("click", function () {
+		$("html, body").animate({
+			scrollTop: $(".contact").offset().top - 90
+		}, 700);
+	});
+
+	/* ---------------------------------------------------
+	   7 - MixItUp Plugin in Portfolio Section
+	----------------------------------------------------- */
+	$(".projects").mixItUp();
+
+	/* ---------------------------------------------------
+		8 - Contact Form Validation
+	----------------------------------------------------- */
+	var contactForm = $("#contact-form"),
+		submitBtn = $(".submit-btn"),
+		formResponse = $(".form-response");
+	contactForm.validator().on("submit", function (e) {
+		if (e.isDefaultPrevented()) {
+			formResponse.text("Sorry, you didn't fill the form.").fadeIn(1000);
+		} else {
+			e.preventDefault();
+			submitForm();
+		}
+	});
+	// Submit Form
+	function submitForm() {
+		// Some Variables
+		var name = $("#name").val(),
+			mail = $("#mail").val(),
+			message = $("#message").val();
+		// Ajax    
+		$.ajax({
+			type: "POST",
+			url: "php/contact.php",
+			data: "name=" + name + "&mail=" + mail + "&message=" + message,
+			beforeSend: function (text) {
+				submitBtn.html("Sending...");
+				formResponse.fadeOut(500).text("");
+			},
+			success: function (text) {
+				if (text == "success") {
+					contactForm[0].reset();
+					formResponse.text("Thanks! Your message sent correctly.").fadeIn(1000);
+					submitBtn.html("Send Message");
+				} else {
+					formResponse.text(text).fadeIn(1000);
+				}
+			}
+		});
+	}
+	// Moving placeholder on focus on any input in contact-me section //
+	$(".contact .form-control").focusout(function () {
+		var textValue = $(this).val();
+		if (textValue === "") {
+			$(this).removeClass("has-value");
+		} else {
+			$(this).addClass("has-value");
+		}
+	});
+
 })(jQuery);
-               /* ---------------------------------------------------
-		           9 - Owl Carousel in Testimonials Section
-	           ----------------------------------------------------- */  
-(function($){
-                $("#owl-demo").owlCarousel({
-                navigation:true, // Show next and prev buttons
-                slideSpeed:500,
-                paginationSpeed:500,
-                items:1, 
-	            pagination:false,
-                itemsDesktop:false,
-                itemsDesktopSmall:false,
-                itemsTablet:false,
-                itemsMobile:false,
-	            navigation:false,
-	            autoPlay:true
-                });
+/* ---------------------------------------------------
+	9 - Owl Carousel in Testimonials Section
+----------------------------------------------------- */
+(function ($) {
+	$("#owl-demo").owlCarousel({
+		navigation: true, // Show next and prev buttons
+		slideSpeed: 500,
+		paginationSpeed: 500,
+		items: 1,
+		pagination: false,
+		itemsDesktop: false,
+		itemsDesktopSmall: false,
+		itemsTablet: false,
+		itemsMobile: false,
+		navigation: false,
+		autoPlay: true
+	});
 })(jQuery);
 
 /* ---------------------------------------------------
@@ -339,7 +339,7 @@ function init() {
 		content: "Dupinder Aulakh"		// Change to your text
 	});
 
-	google.maps.event.addListener(marker, "click", function() {
+	google.maps.event.addListener(marker, "click", function () {
 		infowindow.open(map, marker);
 	});
 }
